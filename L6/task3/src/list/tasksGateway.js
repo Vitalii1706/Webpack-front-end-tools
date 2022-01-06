8c73180bbc6af7a23eb8cff0df8df5252e188b81
Vitalii1706 <kitral.v.o@gmail.com>
2022-01-06 13:59:29 +0200

    1

---
+++ L6/task3/src/list/tasksGateway.js
@@ -0,0 +1,31 @@
+const baseUrl = "https://61c732c3903185001754733f.mockapi.io/api/v1/tasks";
+
+export const getTasksList = () => {
+  return fetch(baseUrl).then((response) => response.json());
+};
+
+export const createTask = (taskData) => {
+  return fetch(baseUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(taskData),
+  });
+};
+
+export const updateTask = (taskId, updatedTaskData) => {
+  return fetch(`${baseUrl}/${taskId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(updatedTaskData),
+  });
+};
+
+export const deleteTask = (taskId) => {
+  return fetch(`${baseUrl}/${taskId}`, {
+    method: "DELETE",
+  });
+};
